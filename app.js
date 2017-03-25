b1312391be0962f971745207d5a8b2a3bd5cc6bd
Himanshu Tomar <htomar@sapient.com>
2017-03-25 00:20:24 +0000

    Added Twitter handle and Sentiment Processing

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoConfig = require('./mongoconfig')
+var twitterAPI = require('./modules/twitter')
 
 var index = require('./routes/index');
 var users = require('./routes/users');
