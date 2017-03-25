14b358f8241592462e26189b06ef31791e751178
Rishi Pratap Singh <rsingh24@sapient.com>
2017-03-25 00:13:45 +0000

    DB Connect

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoConfig = require('./mongoconfig')
+var mongoConfig = require('./mongoconfig');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var mongo_data = require('mongo-data');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
