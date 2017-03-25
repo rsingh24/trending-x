c47ebab23967e98396eda4bee4a4aaef2a0f5ffd
Himanshu Tomar <htomar@sapient.com>
2017-03-25 03:44:35 +0000

    Added support for calculating distance

---
+++ modules/haversine.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const haversine = require('haversine')
+
+module.exports = {
+    getDistanceInMiles =  function(startCoordinates, endCoordinates) {
+        var distanceInMiles = haversine(start, end, {unit: 'mile'});
+        console.log('Distance in Miles :' + distanceInMiles);
+        return haversine(start, end, {unit: 'mile'});
+    }
+};
